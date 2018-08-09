73ebce0ec3f14547367082c972a2dcf0d945707c
Simon Thiboutôt <simonthiboutot@hotmail.com>
2018-08-09 14:00:55 -0400

    Add Free Google Translate API reference in comment.

--- lib/google_translate.rb
+++ lib/google_translate.rb
@@ -7,5 +7,7 @@ BASE_URL = 'https://translate.googleapis.com/translate_a/single'.freeze
 module GoogleTranslate
   def self.translate(text, options)
     # TODO: Implement this method. You can add more methods if you want.
+    # Check out https://ctrlq.org/code/19909-google-translate-api it explains
+    # how to use the Free Google Translate API.
   end
 end
